f3c27f91da975056c3e79fc5bb1cdcd5cfc61e20
Hoang <manhhoangf1@gmail.com[D[C[D[D[D[D[D[D[D[D[D[D[D[D[C[C[C[C[C[C[C[C[C[C[C[C>
2025-07-31 16:27:05 +0200

    Add: function buttons

--- js-dom/js-dom-and-events_dark-mode/js/index.js
+++ js-dom/js-dom-and-events_dark-mode/js/index.js
@@ -1,3 +1,25 @@
 console.clear();
 
 const bodyElement = document.querySelector('[data-js="body"]');
+const darkMode = document.querySelector('[data-js="dark-mode-button"]');
+const lightMode = document.querySelector('[data-js="light-mode-button"]');
+const toggleMode = document.querySelector('[data-js="toggle-button"]');
+
+
+darkMode.addEventListener('click', () => {
+    bodyElement.classList.remove("button--toggle")
+    bodyElement.classList.remove("button--light")
+    bodyElement.classList.add("button--dark")
+})
+
+lightMode.addEventListener('click', () => {
+    bodyElement.classList.remove("button--toggle")
+    bodyElement.classList.remove("button--dark")
+    bodyElement.classList.add("button--light")
+})
+
+toggleMode.addEventListener('click', () => {
+    bodyElement.classList.remove("button--light")
+    bodyElement.classList.remove("button--dark")
+    bodyElement.classList.toggle("button--toggle")
+})
