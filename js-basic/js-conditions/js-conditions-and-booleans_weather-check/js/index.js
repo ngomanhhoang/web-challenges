0466b916ae154757fceeea2e4939b7c74c4af9db
Hoang <manhhoangf1@gmail.com[D[C[D[D[D[D[D[D[D[D[D[D[D[D[C[C[C[C[C[C[C[C[C[C[C[C>
2025-08-01 12:05:21 +0200

    Delete file

--- js-basic/js-conditions/js-conditions-and-booleans_weather-check/js/index.js
+++
@@ -1,5 +0,0 @@
-// Change this value to test different "weather" conditions.
-const weather = "rainy"
-
-// Adjust the "temperature" value to trigger different messages (for Part 2 only).
-const temperature = 22; 
