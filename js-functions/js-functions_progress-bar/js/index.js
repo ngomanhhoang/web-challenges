e40db7c4ddd95cfc6d2a2e76d6b9a472ae6e16c7
Hoang <manhhoangf1@gmail.com[D[C[D[D[D[D[D[D[D[D[D[D[D[D[C[C[C[C[C[C[C[C[C[C[C[C>
2025-08-11 15:16:16 +0200

    Add: progress bar

--- js-functions/js-functions_progress-bar/js/index.js
+++ js-functions/js-functions_progress-bar/js/index.js
@@ -2,4 +2,15 @@ console.clear();
 
 const progressBar = document.querySelector('[data-js="progress-bar"]');
 
-function calculateScrollPercentage() {}
+function calculateScrollPercentage() {
+    const currentPosition = window.scrollY
+    const visibleHeight = window.innerHeight
+    const totalHeight = document.body.clientHeight
+    let percentage = currentPosition / (totalHeight - visibleHeight) * 100
+    return percentage
+}
+
+window.addEventListener("scroll", () => {
+    const percentage = calculateScrollPercentage()
+    progressBar.style.width = percentage + '%'
+})
