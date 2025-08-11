ddd6291f5ba24bb3eb3b30fb24567e3df71d76fc
Hoang <manhhoangf1@gmail.com[D[C[D[D[D[D[D[D[D[D[D[D[D[D[C[C[C[C[C[C[C[C[C[C[C[C>
2025-08-11 11:10:40 +0200

    Add: param extra

--- js-functions/js-functions_functions-with-parameters-extra/js/index.js
+++ js-functions/js-functions_functions-with-parameters-extra/js/index.js
@@ -6,8 +6,9 @@ console.clear();
    The formula is: square = number * number
 */
 
-function printSquare(number) {}
-
+function printSquare(number) {
+   console.log("Result: " + number*number)
+}
 printSquare(3);
 printSquare(5);
 
@@ -18,9 +19,12 @@ printSquare(5);
 */
 
 // Uncomment the following function calls and implement the function `printCircumference`.
+function printCircumference(radius) {
+   console.log("Result: " + Math.floor(2*Math.PI*radius))
+}
 
-// printCircumference(4);
-// printCircumference(6);
+printCircumference(4);
+printCircumference(6);
 
 /*
 3: Write a function that accepts the width and length of a rectangle
@@ -31,6 +35,9 @@ printSquare(5);
 */
 
 // Uncomment the following function calls and implement the function `printRectangleArea`.
+function printRectangleArea(width, length) {
+   console.log("The area of the rectangle is: " + width*length)
+}
 
-// printRectangleArea(5, 7);
-// printRectangleArea(3, 4);
+printRectangleArea(5, 7);
+printRectangleArea(3, 4);
