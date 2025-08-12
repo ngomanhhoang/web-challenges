858fb2de8d46ec31d4f82b72dadded2d8a2fe446
Hoang <manhhoangf1@gmail.com[D[C[D[D[D[D[D[D[D[D[D[D[D[D[C[C[C[C[C[C[C[C[C[C[C[C>
2025-08-12 17:00:17 +0200

    Add: checkbox

--- js-forms/js-forms_checkbox-input/js/index.js
+++ js-forms/js-forms_checkbox-input/js/index.js
@@ -3,6 +3,7 @@ console.clear();
 const form = document.querySelector('[data-js="form"]');
 const tosError = document.querySelector('[data-js="tos-error"]');
 const tosCheckbox = document.querySelector('[data-js="tos"]');
+const success = document.querySelector('[data-js="success"]');
 
 function hideTosError() {
   tosError.setAttribute("hidden", "");
@@ -12,13 +13,35 @@ function showTosError() {
   tosError.removeAttribute("hidden");
 }
 
+function hideSuccess() {
+  success.setAttribute("hidden", "");
+}
+
+function showSuccess() {
+  success.removeAttribute("hidden");
+}
+hideSuccess();
+hideTosError();
+tosCheckbox.addEventListener("input", (e) => {
+  if (e.target.checked) {
+    hideTosError();
+  } else {
+    showTosError();
+  }
+});
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-
+  if (tosCheckbox.checked) {
+    showSuccess();
+  } else {
+    hideSuccess();
+    showTosError()
+    return;
+  }
   // --v-- write your code here --v--
 
   // --^-- write your code here --^--
 
   // eslint-disable-next-line no-alert
-  alert("Form submitted");
+  // alert("Form submitted");
 });
