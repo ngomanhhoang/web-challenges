1861470780dee458d074178bc4de641c9398a781
Hoang <manhhoangf1@gmail.com[D[C[D[D[D[D[D[D[D[D[D[D[D[D[C[C[C[C[C[C[C[C[C[C[C[C>
2025-08-12 16:20:33 +0200

    Add: submit form

--- js-forms/js-forms_handle-form-submit/js/index.js
+++ js-forms/js-forms_handle-form-submit/js/index.js
@@ -1,3 +1,17 @@
 console.clear();
 
 const form = document.querySelector('[data-js="form"]');
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const formData = new FormData(e.target);
+  const data = Object.fromEntries(formData);
+  
+  e.target.firstName.focus();
+  console.log(data);
+
+  const ageBadness = Number(e.target.age.value) + Number(e.target.badness.value)
+  console.log(`The age-badness-sum of ${e.target.firstName.value} is ${ageBadness}`)
+  e.target.reset()
+});
