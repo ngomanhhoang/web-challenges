adb90c026fa66debeb52dc777dd51c7dcf4ebbe1
Hoang <manhhoangf1@gmail.com[D[C[D[D[D[D[D[D[D[D[D[D[D[D[C[C[C[C[C[C[C[C[C[C[C[C>
2025-08-13 13:05:27 +0200

    Add: social post

--- js-createElement/js-createelement_social-media-post/js/index.js
+++ js-createElement/js-createelement_social-media-post/js/index.js
@@ -10,3 +10,29 @@ likeButton.addEventListener("click", handleLikeButtonClick);
 
 // Exercise:
 // Use document.createElement() and append another social media post to the body.
+
+const article = document.createElement("article")
+const p = document.createElement("p")
+const footer = document.createElement("footer")
+const span = document.createElement("span")
+const button = document.createElement("button")
+
+article.classList.add("post")
+p.classList.add("post__content")
+footer.classList.add("post__footer")
+span.classList.add("post__username")
+button.classList.add("post__button")
+button.dataset.js = "like-button"
+
+p.textContent = "Lorem ipsum dolor, sit amet consectetur adipisicing elit."
+span.textContent = "@username"
+button.textContent = "♥ Like"
+
+button.addEventListener("click", handleLikeButtonClick)
+
+
+article.append(p, footer)
+footer.append(span, button)
+document.body.append(article)
+
+
