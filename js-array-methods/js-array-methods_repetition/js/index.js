7e0f6d85cef027fc28601e7db77b1bd5fbad1e99
Hoang <manhhoangf1@gmail.com[D[C[D[D[D[D[D[D[D[D[D[D[D[D[C[C[C[C[C[C[C[C[C[C[C[C>
2025-08-19 09:03:00 +0200

    Add: recipes

--- js-array-methods/js-array-methods_repetition/js/index.js
+++ js-array-methods/js-array-methods_repetition/js/index.js
@@ -35,18 +35,28 @@ const recipes = [
  1: `map` exercises
 */
 
-const onlyTitles = null; // ['Crepes', ...]
+const onlyTitles = recipes.map((recipe) => recipe.title)
+console.log(onlyTitles);
+ // ['Crepes', ...]
 
-const titlesWithDuration = null; // ['Crepes (60min)', ...]
+const titlesWithDuration = recipes.map((recipe) => recipe.title + " (" + recipe.duration + "min)")
+console.log(titlesWithDuration);
 
-const timePerServing = null; // [20, 10, 432, 12]
+// ['Crepes (60min)', ...]
+
+const timePerServing = recipes.map((recipe) => recipe.duration / recipe.servings)
+console.log(timePerServing);
+ // [20, 10, 432, 12]
 
 // EXTRA:
 
 // HINT: use first map() and then Array.prototype.join() with "method chaining" like so:
 // myArray.map(...).join(', ');
 
-const allTitlesInOneString = null; // 'Crepes, Scrambled Eggs, ...'
+const allTitlesInOneString = recipes.map((recipe) => recipe.title).join(", ")
+console.log(allTitlesInOneString);
+
+// 'Crepes, Scrambled Eggs, ...'
 
 // For more information how to join an array,
 // see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/join#joining_an_array_four_different_ways
@@ -55,11 +65,16 @@ const allTitlesInOneString = null; // 'Crepes, Scrambled Eggs, ...'
  2: `filter` exercises
 */
 
-const recipesThatOnlyTake60minutesOrLess = null;
+const recipesThatOnlyTake60minutesOrLess = recipes.filter((recipe) => recipe.duration <= 60)
+console.log(recipesThatOnlyTake60minutesOrLess);
 
-const allRecipesWithMoreThan2Servings = null;
 
-const allRecipesWithTitlesLongerThan12Characters = null;
+const allRecipesWithMoreThan2Servings = recipes.filter((recipe) => recipe.servings > 2)
+console.log(allRecipesWithMoreThan2Servings);
+
+const allRecipesWithTitlesLongerThan12Characters = recipes.filter((recipe) => recipe.title.length > 12)
+console.log(allRecipesWithTitlesLongerThan12Characters);
+
 
 export {
   onlyTitles,
