9f1f1fbd0104d102e9f00d627eafed33ae75753e
Hoang <manhhoangf1@gmail.com[D[C[D[D[D[D[D[D[D[D[D[D[D[D[C[C[C[C[C[C[C[C[C[C[C[C>
2025-08-21 12:52:45 +0200

    Add: arrays

--- js-modern-syntax/js-modern-syntax_destructuring-arrays/js/index.js
+++ js-modern-syntax/js-modern-syntax_destructuring-arrays/js/index.js
@@ -7,6 +7,9 @@ Make sure to export your variable as shown in the example below to make the test
 */
 
 const item = ["Egg", 0.25, 12];
+export const [name, price, quantity] = item
+console.log(name, price, quantity);
+
 
 // Example: export const [ value1, value2 ] = array;
 
@@ -16,6 +19,9 @@ Use array destructuring to extract the variables `firstNameOfChris`, `lastNameOf
 */
 
 const personChris = [12, "Chris", "Owen"];
+export const [ageOfChris,firstNameOfChris, lastNameOfChris] = personChris
+console.log(ageOfChris,firstNameOfChris, lastNameOfChris);
+
 
 /*
 EXERCISE 3
@@ -24,6 +30,9 @@ Ensure no unused variables remain.
 */
 
 const personAlex = ["Alex", 12, "Main"];
+export const [firstNameOfAlex, age, lastNameOfAlex] = personAlex
+console.log(firstNameOfAlex, lastNameOfAlex);
+
 
 /*
 EXERCISE 4
@@ -32,6 +41,9 @@ as variable called `lastName`.
 */
 
 const students = ["Christina", "Jon", "Alexandare"];
+export const [a,b,lastName] = students
+console.log(lastName);
+
 
 /*
 EXERCISE 5
@@ -40,6 +52,8 @@ Assign each name to a variable called "student1" through "student5", in the give
 */
 
 const nestedStudents = ["Chris", ["Ahmad", "Antigoni"], ["Toby", "Sam"]];
+const [student1, [student2, student3], [student4, student5]] = nestedStudents
+console.log(student1,student2,student3,student4,student5);
 
 /*
 EXERCISE 6
@@ -49,10 +63,11 @@ Keep in mind that you need to export the variable `result` to make the test work
 
 const values = [1, 6, 7, 9, 12, 5, 4];
 
-const result = add(1); // Spread values inside this function call
+export const result = add(...values); // Spread values inside this function call
 
 function add(...values) {
   return values.reduce(
     (previousValue, currentValue) => previousValue + currentValue
   );
 }
+console.log(result);
