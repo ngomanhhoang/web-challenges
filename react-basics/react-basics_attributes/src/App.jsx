83930bdee77303349377cb44968d9f65bcdebd82
Hoang <manhhoangf1@gmail.com[D[C[D[D[D[D[D[D[D[D[D[D[D[D[C[C[C[C[C[C[C[C[C[C[C[C>
2025-08-25 12:03:24 +0200

    Add: attributes

--- react-basics/react-basics_attributes/src/App.jsx
+++ react-basics/react-basics_attributes/src/App.jsx
@@ -1,5 +1,14 @@
 import "./styles.css";
 
 export default function App() {
-  return <article>Replace me with an article</article>;
+  return Article()
+}
+
+function Article() {
+  return <article>
+    <h2 className="article__title">Wikipedia</h2>
+    <label htmlFor="search">Type your idee: </label>
+    <input type="text" id="search" />
+    <a href="https://www.wikipedia.org/" className="article__link">Click here !!</a>
+  </article>
 }
