91bbcc04fa604436013fe1883c5c80ce72faa03a
Hoang <manhhoangf1@gmail.com[D[C[D[D[D[D[D[D[D[D[D[D[D[D[C[C[C[C[C[C[C[C[C[C[C[C>
2025-08-25 16:43:44 +0200

    Add: buttons

--- react-props/react-props_buttons/src/App.jsx
+++ react-props/react-props_buttons/src/App.jsx
@@ -1,3 +1,33 @@
-export default function App() {
-  return <h1>Replace me with your component!</h1>;
+export default function App(onClick) {
+  return (
+    <Button
+      handleClick={onClick}
+      color={"white"}
+      disabled={false}
+      text={"Hello World"}
+    />
+  );
+}
+
+function Button({ color, disabled, text }) {
+  return (
+    <button
+      onClick={handleClick}
+      style={{
+        color,
+        backgroundColor: "black",
+        fontSize: "30px",
+        padding: "10px 20px",
+        borderRadius: "8px",
+        cursor: "pointer",
+      }}
+      disabled={disabled}
+    >
+      {text}
+    </button>
+  );
+
+  function handleClick() {
+    console.log("You clicked me!");
+  }
 }
