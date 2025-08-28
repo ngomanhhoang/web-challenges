1b1b2cb5459021f47bc40ca2be3fa005b8d1491f
Hoang <manhhoangf1@gmail.com[D[C[D[D[D[D[D[D[D[D[D[D[D[D[C[C[C[C[C[C[C[C[C[C[C[C>
2025-08-28 13:34:17 +0200

    Add: users

--- react-arrays/react-with-arrays_users/src/components/Card.jsx
+++ react-arrays/react-with-arrays_users/src/components/Card.jsx
@@ -1,14 +1,16 @@
 import "./Card.css";
 import Tag from "./Tag";
 import Button from "./Button";
-function Card({ user }) {
+import { USERS } from "../db";
+function Card({ name, roles, about }) {
   return (
     <article className="card">
-      <h2>{user.name}</h2>
+      <h2>{name}</h2>
       <ul className="card__taglist">
-        <Tag tag={user.roles[0]} />
+        {roles.map((role) => <Tag key={role} tag={role} />)}
+        
       </ul>
-      <p>{user.about}</p>
+      <p>{about}</p>
       <Button>edit</Button>
       <Button buttonType="danger">invoice</Button>
     </article>
