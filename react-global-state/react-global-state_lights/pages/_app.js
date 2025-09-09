33b8ef2bd5a3622ead5d9dec62b6914a9f75bd42
Hoang <manhhoangf1@gmail.com[D[C[D[D[D[D[D[D[D[D[D[D[D[D[C[C[C[C[C[C[C[C[C[C[C[C>
2025-09-09 20:03:07 +0200

    Add: global state lights

--- react-global-state/react-global-state_lights/pages/_app.js
+++ react-global-state/react-global-state_lights/pages/_app.js
@@ -1,11 +1,86 @@
 import GlobalStyle from "../styles";
 import Layout from "../components/Layout";
+import { useState } from "react";
+
+const initialLights = [
+  {
+    id: 1,
+    name: "Living Room",
+    isOn: false,
+  },
+  {
+    id: 2,
+    name: "Kitchen",
+    isOn: false,
+  },
+  {
+    id: 3,
+    name: "Bedroom",
+    isOn: false,
+  },
+  {
+    id: 4,
+    name: "Bathroom",
+    isOn: false,
+  },
+  {
+    id: 5,
+    name: "Garage",
+    isOn: false,
+  },
+  {
+    id: 6,
+    name: "Porch",
+    isOn: false,
+  },
+  {
+    id: 7,
+    name: "Garden",
+    isOn: false,
+  },
+  {
+    id: 8,
+    name: "Office",
+    isOn: false,
+  },
+];
 
 export default function App({ Component, pageProps }) {
+  const [lights, setLights] = useState(initialLights);
+
+  function toggleLight(id) {
+    setLights(
+      lights.map((light) =>
+        light.id === id ? { ...light, isOn: !light.isOn } : light
+      )
+    );
+  }
+  function handleOn() {
+    setLights(
+      lights.map((light) => {
+        return { ...light, isOn: true };
+      })
+    );
+  }
+
+  function handleOff() {
+    setLights(
+      lights.map((light) => {
+        return { ...light, isOn: false };
+      })
+    );
+  }
+  const isDimmed = lights.every((light) => !light.isOn)
   return (
-    <Layout>
+    <Layout isDimmed={isDimmed}>
       <GlobalStyle />
-      <Component {...pageProps} />
+      <Component
+        {...pageProps}
+        lights={lights}
+        toggleLight={toggleLight}
+        turnAllOn={handleOn}
+        turnAllOff={handleOff}
+      />
     </Layout>
   );
 }
