998938741f56749c6245b72844d8a490284b8258
Hoang <manhhoangf1@gmail.com[D[C[D[D[D[D[D[D[D[D[D[D[D[D[C[C[C[C[C[C[C[C[C[C[C[C>
2025-09-09 08:21:47 +0200

    Add: scorekeeper testing

--- react-component-testing/react-component-testing_scorekeeper/components/GameForm/GameForm.test.js
+++ react-component-testing/react-component-testing_scorekeeper/components/GameForm/GameForm.test.js
@@ -8,10 +8,46 @@ jest.mock("next/router", () => ({
   },
 }));
 
-test("renders two input fields and a button", () => {});
+test("renders two input fields and a button", () => {
+  const onCreateGame = jest.fn();
+  render(<GameForm onCreateGame={onCreateGame} />);
+  expect(screen.getAllByRole("textbox")).toHaveLength(2);
+  const button = screen.getByRole("button", { name: /create game/i });
+  expect(button).toBeInTheDocument();
+});
 
-test("renders a form with the accessible name 'Create a new game'", () => {});
+test("renders a form with the accessible name 'Create a new game'", () => {
+  const onCreateGame = jest.fn();
+  render(<GameForm onCreateGame={onCreateGame} />);
+  expect(
+    screen.getByRole("form", { name: /create a new game/i })
+  ).toBeInTheDocument();
+});
 
-test("submits the correct form data when every field is filled out", async () => {});
+test("submits the correct form data when every field is filled out", async () => {
+  const onCreateGame = jest.fn();
+  const user = userEvent.setup();
 
-test("does not submit form if one input field is left empty", async () => {});
+  render(<GameForm onCreateGame={onCreateGame} />);
+  const firstInput = screen.getByLabelText("Name of game");
+  const secondInput = screen.getByLabelText("Player names, separated by comma");
+  await user.type(firstInput, "My Game");
+  await user.type(secondInput, "Alice, Bob");
+  const submitButton = screen.getByRole("button", { name: /create game/i });
+  expect(submitButton).toBeInTheDocument();
+  await user.click(submitButton);
+  expect(onCreateGame).toHaveBeenCalledWith({
+    nameOfGame: "My Game",
+    playerNames: ["Alice", "Bob"],
+  });
+});
+
+test("does not submit form if one input field is left empty", async () => {
+  const user = userEvent.setup();
+
+  const onCreateGame = jest.fn();
+  render(<GameForm onCreateGame={onCreateGame} />);
+  const submitButton = screen.getByRole("button", { name: /create game/i });
+  await user.click(submitButton);
+  expect(onCreateGame).not.toHaveBeenCalled();
+});
--- react-component-testing/react-component-testing_scorekeeper/components/Player/Player.test.js
+++ react-component-testing/react-component-testing_scorekeeper/components/Player/Player.test.js
@@ -2,6 +2,42 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Player from ".";
 
-test("renders player information and two buttons", () => {});
+test("renders player information and two buttons", () => {
+  const handleDecrease = jest.fn();
+  const handleIncrease = jest.fn();
 
-test("calls callbacks when increasing or decreasing score", async () => {});
+  render(
+    <Player
+      name="playerNames"
+      score={4}
+      onDecreasePlayerScore={handleDecrease}
+      onIncreasePlayerScore={handleIncrease}
+    />
+  );
+
+  expect(screen.getByText("playerNames")).toBeInTheDocument();
+  expect(screen.getByText("4")).toBeInTheDocument();
+  expect(screen.getAllByRole("button")).toHaveLength(2);
+});
+
+test("calls callbacks when increasing or decreasing score", async () => {
+  const handleDecrease = jest.fn();
+  const handleIncrease = jest.fn();
+
+  const user = userEvent.setup();
+  render(
+    <Player
+      name="playerNames"
+      score={4}
+      onDecreasePlayerScore={handleDecrease}
+      onIncreasePlayerScore={handleIncrease}
+    />
+  );
+  const decreaseBtn = screen.getByLabelText("Decrease Score");
+  const increaseBtn = screen.getByLabelText("Increase Score");
+
+  await user.click(decreaseBtn);
+  await user.click(increaseBtn);
+  expect(handleDecrease).toHaveBeenCalledTimes(1);
+  expect(handleIncrease).toHaveBeenCalledTimes(1);
+});
