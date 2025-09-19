8fffbb10b7d42bfaa25ad8e4b1ed0e9aec4fc942
Hoang <manhhoangf1@gmail.com[D[C[D[D[D[D[D[D[D[D[D[D[D[D[C[C[C[C[C[C[C[C[C[C[C[C>
2025-09-19 13:53:21 +0200

    Add: update and delete fish

--- backend-update-and-delete/backend-update-and-delete_products/components/ProductForm.js
+++ backend-update-and-delete/backend-update-and-delete_products/components/ProductForm.js
@@ -1,36 +1,11 @@
 import styled from "styled-components";
-import useSWR from "swr";
+
 import StyledButton from "@/components/Button";
 
-export default function ProductForm() {
-  const { mutate } = useSWR("/api/products");
-
-  async function handleSubmit(event) {
-    event.preventDefault();
-
-    const formData = new FormData(event.target);
-    const productData = Object.fromEntries(formData);
-
-    const response = await fetch("/api/products", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(productData),
-    });
-
-    if (!response.ok) {
-      console.error(response.status);
-      return;
-    }
-
-    mutate();
-    event.target.reset();
-  }
-
+export default function ProductForm({onSubmit, isUpdate}) {
   return (
-    <StyledForm onSubmit={handleSubmit}>
-      <StyledHeading>Add a new Fish</StyledHeading>
+    <StyledForm onSubmit={onSubmit}>
+      <StyledHeading>{isUpdate ? "Add a new Fish" : "Update a fish"}</StyledHeading>
       <StyledLabel htmlFor="name">
         Name:
         <input type="text" id="name" name="name" />
